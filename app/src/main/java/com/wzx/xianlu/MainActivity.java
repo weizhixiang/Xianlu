6bdeac4207f24c9dc07803bf1c516d073e4f5acd
weizhixiang <weizhixiang1234@126.com>
2016-03-27 12:25:24 +0800

    Initial commit

---
+++ app/src/main/java/com/wzx/xianlu/MainActivity.java
@@ -0,0 +1,453 @@
+package com.wzx.xianlu;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.Tile;
+import com.amap.api.maps.model.TileOverlay;
+import com.amap.api.maps.model.TileOverlayOptions;
+import com.amap.api.maps.model.TileProvider;
+import com.amap.api.maps.model.UrlTileProvider;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.snappydb.DB;
+import com.snappydb.DBFactory;
+import com.snappydb.SnappyDB;
+import com.snappydb.SnappydbException;
+import com.tandong.sa.zUImageLoader.cache.disc.impl.UnlimitedDiscCache;
+import com.tandong.sa.zUImageLoader.core.DisplayImageOptions;
+import com.tandong.sa.zUImageLoader.core.ImageLoader;
+import com.tandong.sa.zUImageLoader.core.ImageLoaderConfiguration;
+import com.tandong.sa.zUImageLoader.core.assist.FailReason;
+import com.tandong.sa.zUImageLoader.core.listener.ImageLoadingListener;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity implements LocationSource,
+        AMapLocationListener {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private MapView mapView;
+    private AMap aMap;
+    private OnLocationChangedListener mListener;
+    private AMapLocationClient mlocationClient;
+    private AMapLocationClientOption mLocationOption;
+    private TextView textView;
+    protected ImageLoader imageLoader;
+    private ImageView imageView;
+    private DisplayImageOptions options;
+    private DB nappydb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mapView = (MapView) findViewById(R.id.map);
+        textView = (TextView) findViewById(R.id.textView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        mapView.onCreate(savedInstanceState);
+        imageLoader = ImageLoader.getInstance();
+        imageConfig(this);//图片加载初始化
+        init();
+        try {
+            nappydb = new SnappyDB.Builder(this)
+                    .directory(Environment.getExternalStorageDirectory().getAbsolutePath()) //optional
+                    .name("books")//optional
+                    .build();
+
+        } catch (SnappydbException e) {
+            e.printStackTrace();
+        }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+    }
+
+    private void imageConfig(Context context) {
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration
+                .Builder(context)
+                .memoryCacheExtraOptions(512, 512) // max width, max height，即保存的每个缓存文件的最大长宽
+                .threadPoolSize(3)//线程池内加载的数量
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                        //.denyCacheImageMultipleSizesInMemory()
+                        //.memoryCache(new UsingFreqLimitedMemoryCache(10 * 1024 * 1024)) // You can pass your own memory cache implementation/你可以通过自己的内存缓存实现
+                        //.memoryCacheSize(20 * 1024 * 1024)
+                .diskCacheSize(50 * 1024 * 1024)
+                        // .diskCacheFileNameGenerator(new Md5FileNameGenerator())//将保存的时候的URI名称用MD5 加密
+                        // .tasksProcessingOrder(QueueProcessingType.LIFO)
+                        // .diskCacheFileCount(100) //缓存的文件数量
+                .diskCache(new UnlimitedDiscCache(new File("/mnt/sdcard/amap/cache/")))//自定义缓存路径
+                        //.defaultDisplayImageOptions(DisplayImageOptions.createSimple())
+                        //.imageDownloader(new BaseImageDownloader(context, 5 * 1000, 30 * 1000)) // connectTimeout (5 s), readTimeout (30 s)超时时间
+                        .writeDebugLogs() // Remove for release app
+                .build();//开始构建
+        ImageLoader.getInstance().init(config);
+        options = new DisplayImageOptions.Builder()
+                .showImageOnLoading(R.mipmap.ic_launcher)
+                        //.cacheInMemory(true)//设置下载的图片是否缓存在内存中
+                .cacheOnDisk(true)//设置下载的图片是否缓存在SD卡中
+                .build();//构建完成
+    }
+
+    public byte[] Bitmap2Bytes(Bitmap bm) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 80, baos);
+        return baos.toByteArray();
+    }
+
+    private void init() {
+        if (aMap == null) {
+            aMap = mapView.getMap();
+            setUpMap();
+            final TileOverlay tileOverlay;
+            final String url = "http://mt0.google.cn/vt/lyrs=y@198&hl=zh-CN&gl=cn&src=app&x=%d&y=%d&z=%d&scale=2&s=";
+            final String imageUri = "file:///mnt/sdcard/image.png";
+            final TileProvider tileProvider1 = new TileProvider() {
+                @Override
+                public Tile getTile(final int x, final int y, final int zoom) {
+
+                    try {
+                        if (!nappydb.exists(x + "_" + y + "_" + zoom)) {
+                            imageLoader.loadImage(String.format(url, x, y, zoom), new ImageLoadingListener(){
+
+                                @Override
+                                public void onLoadingStarted(String s, View view) {
+
+                                }
+
+                                @Override
+                                public void onLoadingFailed(String s, View view, FailReason failReason) {
+
+                                }
+
+                                @Override
+                                public void onLoadingComplete(String s, View view, Bitmap bitmap) {
+                                    try {
+                                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, baos);
+                                        Log.e("Amap", String.valueOf(baos.size()));
+                                        nappydb.put(x + "_" + y + "_" + zoom, baos);
+
+                                    } catch (SnappydbException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                }
+
+                                @Override
+                                public void onLoadingCancelled(String s, View view) {
+
+                                }
+                            });
+                            //return new Tile(256, 256, Bitmap2Bytes(imageLoader.loadImageSync(String.format(url, x, y, zoom))));
+                        }
+                    } catch (SnappydbException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        return new Tile(256, 256, nappydb.getObject(x + "_" + y + "_" + zoom, ByteArrayOutputStream.class).toByteArray());
+                    } catch (SnappydbException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+
+                @Override
+                public int getTileWidth() {
+                    return 256;
+                }
+
+                @Override
+                public int getTileHeight() {
+                    return 256;
+                }
+            };
+            final TileProvider tileProvider = new UrlTileProvider(256, 256) {
+
+                public URL getTileUrl(final int x, final int y, final int zoom) {
+                    File file = new File("/mnt/sdcard/amap/cache/" + x + "_" + y + "_" + zoom);
+                    if (!file.exists()) {
+                        try {
+                            imageLoader.loadImage(String.format(url, x, y, zoom), new ImageLoadingListener() {
+                                @Override
+                                public void onLoadingStarted(String s, View view) {
+
+                                }
+
+                                @Override
+                                public void onLoadingFailed(String s, View view, FailReason failReason) {
+
+                                }
+
+                                @Override
+                                public void onLoadingComplete(String s, View view, Bitmap bitmap) {
+                                    File file = new File("/mnt/sdcard/amap/cache/" + x + "_" + y + "_" + zoom);
+                                    BufferedOutputStream bos = null;
+                                    try {
+                                        bos = new BufferedOutputStream(new FileOutputStream(file));
+                                        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, bos);
+                                        bos.flush();
+                                        bos.close();
+
+                                    } catch (FileNotFoundException e) {
+                                        e.printStackTrace();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                }
+
+                                @Override
+                                public void onLoadingCancelled(String s, View view) {
+
+                                }
+                            });
+                            Log.e("AmapErr", "dddd");
+                            return new URL(String.format(url, x, y, zoom));
+
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+
+                    try {
+                        return new URL(String.format("file:///mnt/sdcard/amap/cache/%d_%d_%d", x, y, zoom));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+
+            };
+            if (tileProvider1 != null) {
+                tileOverlay = aMap.addTileOverlay(new TileOverlayOptions()
+                        .tileProvider(tileProvider1)
+                                //.diskCacheEnabled(true)
+                                //.diskCacheDir("/storage/emulated/0/amap/cache")
+                                //.diskCacheSize(100000)
+                        .memoryCacheEnabled(true)
+                        .memCacheSize(100000))
+                ;
+            }
+        }
+    }
+
+    private void setUpMap() {
+        aMap.setLocationSource(this);// 设置定位监听
+        aMap.getUiSettings().setScaleControlsEnabled(true);
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(16));
+        aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        // 设置定位的类型为定位模式 ，可以由定位、跟随或地图根据面向方向旋转几种
+        aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.wzx.xianlu/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.wzx.xianlu/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+        try {
+            nappydb.close();
+        } catch (SnappydbException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+        if (null != mlocationClient) {
+            mlocationClient.onDestroy();
+        }
+
+    }
+
+    @Override
+    public void activate(OnLocationChangedListener listener) {
+        mListener = listener;
+        if (mlocationClient == null) {
+            mlocationClient = new AMapLocationClient(this);
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位监听
+            mlocationClient.setLocationListener(this);
+            //设置为高精度定位模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置定位参数
+            mLocationOption.setInterval(1000);
+            mlocationClient.setLocationOption(mLocationOption);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            mlocationClient.startLocation();
+        }
+    }
+
+    /**
+     * 停止定位
+     */
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mlocationClient != null) {
+            mlocationClient.stopLocation();
+            mlocationClient.onDestroy();
+        }
+        mlocationClient = null;
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+
+        if (mListener != null && amapLocation != null) {
+            if (amapLocation != null
+                    && amapLocation.getErrorCode() == 0) {
+//                mLocationErrText.setVisibility(View.GONE);
+                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+                // Log.e("AmapErr", String.valueOf(amapLocation.getLocationType()));
+                DecimalFormat df = new DecimalFormat(".00000000");
+                textView.setText(df.format(amapLocation.getLongitude()) + "," + df.format(amapLocation.getLatitude()) + ",精确度" + amapLocation.getAccuracy() + "米    模式：" + amapLocation.getProvider());
+            } else {
+                String errText = "定位失败," + amapLocation.getErrorCode() + ": " + amapLocation.getErrorInfo();
+                textView.setText(errText);
+//                mLocationErrText.setVisibility(View.VISIBLE);
+//                mLocationErrText.setText(errText);
+            }
+        }
+    }
+}
